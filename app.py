3ce9155729b08d97779a05210e3228ea9e3a9409
Demian143 <patrickxavier140@gmail.com>
2022-11-13 22:42:30 -0300

    adding db

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from flask_lambda import FlaskLambda
+from flask import Flask
 
-app = FlaskLambda(__name__)
-
-
-def app():
-    """"A aplicação receberá dois parâmetros bucket_name e object_key,
+""""A aplicação receberá dois parâmetros bucket_name e object_key,
     referente a um arquivo CSV em um bucket do s3, via requisição HTTP."""
-    # receber requisição dois parâmetros bucket_name e object_key
-    # acessar o bucket para ler o arquivo
-    pass
+# receber requisição dois parâmetros bucket_name e object_key
+
+# acessar o bucket para ler o arquivo
+
+
+def app_factory():
+    app = Flask(__name__)
+    add_db(app)
+    add_resource(app)
+
+    return app
+
+
+def add_resource(app):
+    from rest.basic_resource import HomeResource
+
+    from flask_restful import Api
+
+    api = Api(app)
+    api.add_resource(HomeResource, '/')
+
+
+def add_db(app):
+    from db.database import db
+    app.config[
+        "SQLALCHEMY_DATABASE_URI"
+    ] = "postgresql://postgres:mysecretpassword@0.0.0.0:5432/postgres"
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+
+if __name__ == '__main__':
+    app_factory().run(debug=True)
